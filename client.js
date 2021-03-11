24f986998ae01ac5a0e9efb7422c6bde081587a4
Minjae Jung <noahjung87@gmail.com>
2021-03-11 08:43:48 +0000

    Constants Module

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 /**
  * Establishes connection with the game server
  */
+ const { IP, PORT } = require('./constants');
  const net = require('net');
+
 
  const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
