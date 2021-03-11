d23441b37f14485bcebc5e1a68187effae964c8e
Minjae Jung <noahjung87@gmail.com>
2021-03-11 04:12:37 +0000

    Connection module done

---
+++ client.js
@@ -0,0 +1,26 @@
+/**
+ * Establishes connection with the game server
+ */
+ const net = require('net');
+
+ const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  conn.on('connect', () => {
+    conn.write('Hello from client Noah!');
+  
+  });
+
+  return conn;
+ }
+
+module.exports = { connect };
