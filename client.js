551218f134b67ef098bc00a2990ceb02090521c2
Minjae Jung <noahjung87@gmail.com>
2021-03-11 04:56:05 +0000

    Move Commands done

--- client.js
+++ client.js
@@ -19,7 +19,20 @@
     console.log("Successfully connected to game server" );
     conn.write('Hello from client Noah!');
     conn.write('Name: MJJ');
+
   });
+
+  // conn.on('connect', () => {
+    
+  //   const myAction = setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50)
+
+  //   setTimeout(() => {
+  //     clearInterval(myAction);
+  //   }, 1000)
+    
+  // });
 
   return conn;
  }
