64f07c634f6825d1c4d375d49010d4a0e457cf3b
Minjae Jung <noahjung87@gmail.com>
2021-03-11 03:53:54 +0000

    client set up done

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  conn.on('connect', () => {
+    conn.write('Hello from client Noah!');
+  
+  });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
