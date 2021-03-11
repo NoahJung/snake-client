29ff4576eef6b0bd77aeaed8c32bbb9cb11011bb
Minjae Jung <noahjung87@gmail.com>
2021-03-11 05:21:02 +0000

    Stdin Setup done

--- client.js
+++ client.js
@@ -37,4 +37,23 @@
   return conn;
  }
 
-module.exports = { connect };
+ const setupInput = function() {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("exiting");
+      process.exit();
+    }
+  });
+ 
+
+  return stdin;
+}
+
+module.exports = { 
+  connect, 
+  setupInput };
